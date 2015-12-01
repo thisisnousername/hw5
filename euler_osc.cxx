264a62ea66d7825778d6565e718dc057242ab69a
thisisnousername <markus.rosendahl@hhu.de>
2015-12-01 11:32:52 +0100

    optimized code

--- euler_osc.cxx
+++ euler_osc.cxx
@@ -19,7 +19,7 @@ int main(){
 	double f[n];					// forward array
 	double b[n];					// backward array
 
-	double temp[n];					// temprorary array
+	double temp;					// temprorary variable
 
 	f[0]=1;						// initial conditions
 	f[1]=0;
@@ -32,17 +32,15 @@ int main(){
 
 		y = cos(dt*(i+1));
 
-		temp[0] = f[0]+f[1]*dt;
-		temp[1] = f[1]-f[0]*dt;
+		temp=f[0];
 
-		f[0] = temp[0];
-		f[1] = temp[1];
+		f[0] = f[0]+f[1]*dt;
+		f[1] = f[1]-temp*dt;
 
-		temp[0] = (b[0]+b[1]*dt)/(1+dt*dt);
-		temp[1] = b[1] - dt*(b[0]+b[1]*dt)/(1+dt*dt);
+		temp=b[0];
 
-		b[0] = temp[0];
-		b[1] = temp[1];
+		b[0] = (b[0]+b[1]*dt)/(1+dt*dt);
+		b[1] = b[1] - dt*(temp+b[1]*dt)/(1+dt*dt);
 
 	cout << i*dt << "\t" << y << "\t" << f[0] << "\t" << b[0] << endl;
 	}
